1c238f03c94431f21325ff10e4295d38f0f7f03f
Rizwan-0905 <bsai23004@itu.edu.pk>
2024-11-15 08:57:07 +0500

    added 3 pages

--- medishield/medishield/views.py
+++ medishield/medishield/views.py
@@ -1,6 +1,16 @@
 # created by Rizwan
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 def home(request):
-    return render(request,'home.html')
+    x=datetime.datetime.now()
+    params={'date':x}
+    return render(request,'home.html',params)
+
+def suppliers(request):
+    return render(request, 'suppliers.html')
+
+
+def new_supp(request):
+    return render (request, 'new_supp.html')
