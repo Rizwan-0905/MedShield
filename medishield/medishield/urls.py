3dffd1d480cd69f302a5f8b484d34f076fcf31d7
Rizwan-0905 <bsai23004@itu.edu.pk>
2024-11-16 23:21:03 +0500

    added html pages

--- medishield/medishield/urls.py
+++ medishield/medishield/urls.py
@@ -19,7 +19,19 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('suppliers/',views.suppliers,name='suppliers'),
-    path('new_supp/',views.new_supp,name='new_supp')
+    path('',views.home,name='home'),# done
+    path('suppliers/',views.suppliers,name='suppliers'), #done
+    path('new_supp/',views.new_supp,name='new_supp'),#done
+    path('bills/',views.bill,name='bills'),#done
+    path('add_bill/',views.add_bill,name='add_bill'),#done
+    path('dead_stock/',views.dead_stock,name='dead_stock'),#done
+    path('customers/',views.customer,name='customer'),# done
+    path('orders/',views.orders,name='orders'),#done
+    path('add_order/',views.add_order,name='add_order'),
+    path('receive_order/',views.receive_order,name='receive_order'),# deal with inventory upon receiving order
+    path('inventory/',views.inventory,name='inventory'),#done
+    path('returns/',views.returns,name='returns'),
+     path('add_return/',views.add_return,name='add_return'),
+    path('complete_returns/',views.complete_returns,name='complete_returns')
+
 ]
